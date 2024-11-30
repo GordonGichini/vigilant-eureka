f13b015f2e44e5c853cd40bcf58eaa7522b105c6
GordonGichini <gordongichini1@gmail.com>
2024-11-30 18:06:04 +0300

    login and register forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import CreatePortfolio from './components/Create'
 import Portfolio from './components/Portfolio'
+import Login from './components/Login'
+import Register from './components/Register'
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Routes>
         <Route path="/home" element={<Portfolio />} />
         <Route path="/create-portfolio" element={<CreatePortfolio />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   )
