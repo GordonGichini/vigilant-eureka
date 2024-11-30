c1d5c9818fb8dd401bf4d84e17037a9456f3f12a
GordonGichini <gordongichini1@gmail.com>
2024-11-30 16:36:47 +0300

    create portfolio form

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import CreatePortfolio from './components/Create'
 import Portfolio from './components/Portfolio'
 
 function App() {
   return (
-    <Portfolio />
+    <Router>
+      <Routes>
+        <Route path="/home" element={<Portfolio />} />
+        <Route path="/create-portfolio" element={<CreatePortfolio />} />
+      </Routes>
+    </Router>
   )
 }
 
